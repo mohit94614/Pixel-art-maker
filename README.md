5c91bdf8fb06eee0f1396d55b14a0ab49b835e7c
Mohit Sharma <mohit94614@gmail.com>
2018-05-01 01:21:14 +0530

    first commit

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Pixel Art Maker Project
 
-## Table of Contents
-
-* [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
-
-To get started, open `designs.js` and start building out the app's functionality.
+ Design.js for javascript file
+ index.html for main html file
+ styles.css for all the css used in the project
 
 For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+## Made using 
+
+1 Bootstrap 
+2 jQuery 
+3 HTML
+4 CSS 
+
+
--- designs.js
+++ designs.js
@@ -1,10 +1,50 @@
-// Select color input
-// Select size input
+let color = "#f00"; //default color (red)
+let gridSize = [15, 15]; //default grid size [width , height]
 
-// When size is submitted by the user, call makeGrid()
+//jQuery wrap
+$(function () {
+    //function called when user submits size
+    function makeGrid() {
+        gridSize[0] = $('#gwidth').val();
+        gridSize[1] = $('#gheight').val();
+        $('#main').empty();
+        for (let r = 0; r < gridSize[1]; ++r) {
+            let row = $('<div class="pixel"></div>');
+            $('#main').append(row);
+            for (let c = 0; c < gridSize[0]; ++c) {
+                row.append('<div class="pcol backcolor" clicked="0"></div>');
+            }
+        }
+    } 
+    //event listener for form submit
+    $("#sizePicker").submit(function (event) {
+        event.preventDefault();
+        makeGrid();
+    });
 
-function makeGrid() {
+    //event listener for color picker
+    $('#colorPicker').change(function () {
+        color = $('#colorPicker').val();
+    });
 
-// Your code goes here!
+    //event delegation for div's inside container
+    $('#main').on('click', '.pcol', function () {
+        if ($(this).attr('clicked') === '0') {
+            $(this).attr('clicked', '1');
+            $(this).removeClass('backcolor');
+            color = $('#colorPicker').val();
+            $(this).css('background-color', color);
+        } else {
+            $(this).attr('clicked', '0');
+            $(this).addClass('backcolor');
+        }
+    });
 
-}
+    $( "#gwidth,#gheight" ).change(function() {
+        makeGrid();
+});
+
+    makeGrid();
+    
+
+});
